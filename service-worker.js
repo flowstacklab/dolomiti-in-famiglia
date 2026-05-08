161cc04e24fbb3b2f7d01b06ce303d2479f2e946
Christian <christiantiralosi@gmail.com>
2026-05-08 18:19:33 +0200

    INIT

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE = 'dolomiti-v1';
+const ASSETS = [
+  './',
+  './dolomiti_itinerary.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      if (r) return r;
+      return fetch(e.request).catch(() => caches.match('./dolomiti_itinerary.html'));
+    })
+  );
+});
